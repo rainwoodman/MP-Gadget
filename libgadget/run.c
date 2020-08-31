1aef2b26cca92e0527960063548ffe332c659217
daviesje <daviesje@student.unimelb.edu.au>
2020-08-31 10:13:22 +1000

    smooth sfr over hubble time, add excursion set flag

--- libgadget/run.c
+++ libgadget/run.c
@@ -460,7 +460,7 @@ run(int RestartSnapNum)
             fof_finish(&fof);
         }
         
-        if(CalcUVBG) {
+        if(All.ExcursionSetFlag && CalcUVBG) {
             calculate_uvbg();
             if(WriteSnapshot) {
                 save_uvbg_grids(SnapshotFileCount);
